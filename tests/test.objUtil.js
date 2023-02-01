8d3f9fbeef5ff7c152b3eb6a11dfd73095340a4c
Pin-Xuan She <snakeneedy@gmail.com>
2023-02-02 02:04:15 +0800

    M  tests/test.objUtil.js

--- tests/test.objUtil.js
+++ tests/test.objUtil.js
@@ -4,6 +4,9 @@ import { jsonCopy } from '../src/objUtil.js';
 export const testJsonCopy = () => (test('testJsonCopy', async () => {
   const basicValues = [null, undefined, true, 10, 3.14, 'abc'];
   basicValues.forEach((val) => (assert(val === jsonCopy(val))));
+  const obj = { abc: 3.14 };
+  const objCopied = jsonCopy(obj);
+  assert(obj.abc === objCopied.abc);
 }));
 
 export default {
