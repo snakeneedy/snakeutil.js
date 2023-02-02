6f12f06f6e2f7e5f6e4ebb8a08c64b2cf7691266
Pin-Xuan She <snakeneedy@gmail.com>
2023-02-03 01:11:20 +0800

    count OK or failed in runTest()
    M  src/testing.js

--- src/testing.js
+++ src/testing.js
@@ -9,8 +9,10 @@ const testJob = (title, callback = async () => {}) => (async (times = DEFAULT_TI
       if (result instanceof Promise) await result;
     }
     console.timeEnd(title);
+    return true;
   } catch (err) {
     console.error(title + ':', err);
+    return false;
   }
 });
 
@@ -19,10 +21,15 @@ export const test = (title, callback = async () => {}) => {
 };
 
 export const runTest = async (times = DEFAULT_TIMES) => {
+  console.log(`=== runTest ${times} times ===`);
+  let countOK = 0, countFailed = 0;
   while (testingSequence.length) {
-    const callback = testingSequence.shift();
-    await callback(times);
+    const toTest = testingSequence.shift();
+    const isOK = await toTest(times);
+    if (isOK) countOK += 1;
+    else countFailed += 1;
   };
+  console.log(`... ${countOK} OK / ${countFailed} failed.`);
 };
 
 export default {
