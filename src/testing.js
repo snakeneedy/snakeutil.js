2436875d2d650a6c43486c1939539029d3114ba3
Pin-Xuan She <snakeneedy@gmail.com>
2023-02-02 20:13:57 +0800

    M  src/testing.js: execute more than once

--- src/testing.js
+++ src/testing.js
@@ -6,11 +6,14 @@ export const assert = (condition, message = '') => {
   if (!condition) throw new AssertionError(message);
 };
 
-export const test = async (title, callback = () => {}) => {
+export const test = async (title, callback = async () => {}, times = 1000) => {
   try {
     const beginTime = performance.now();
-    const result = callback();
-    if (result instanceof Promise) await result;
+    while (times > 0) {
+      const result = callback();
+      if (result instanceof Promise) await result;
+      times -= 1;
+    }
     const endTime = performance.now();
     console.log('-', title, endTime - beginTime, 'ms');
   } catch(err) {
